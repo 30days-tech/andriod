bfc4d7f81435188d6fa328b07885be1fde667ef0
yanchengmeng <960976195@qq.com>
2017-11-21 16:49:28 +0800

    修改PhoneUtil

--- 公共组件/common/src/main/java/com/thirtydays/common/util/PhoneUtil.java
+++ 公共组件/common/src/main/java/com/thirtydays/common/util/PhoneUtil.java
@@ -10,14 +10,32 @@ import android.util.DisplayMetrics;
 
 public class PhoneUtil {
 
+    /**
+     * 获取手机屏幕width
+     *
+     * @param context
+     * @return
+     */
     public static int getScreenWidth(Context context) {
         return context.getResources().getDisplayMetrics().widthPixels;
     }
 
-    public static int getScreenHeigth(Context context) {
+    /**
+     * 获取手机屏幕height
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
         return context.getResources().getDisplayMetrics().heightPixels;
     }
 
+    /**
+     * 获取屏幕widht、height
+     *
+     * @param context
+     * @return
+     */
     public static int[] getScreenSize(Context context) {
         int[] screenSize = new int[2];
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
@@ -25,4 +43,44 @@ public class PhoneUtil {
         screenSize[1] = displayMetrics.heightPixels;
         return screenSize;
     }
+
+    /**
+     * dip转px
+     *
+     * @param context
+     * @param dpValue
+     * @return
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * px转dip
+     *
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 获取手机IMEI号
+     * 需要添加<uses-permission android:name="android.permission.READ_PHONE_STATE"/>权限
+     *
+     * @return
+     */
+    public static String getPhoneIMEI(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (telephonyManager == null) {
+            return "";
+        }
+        String imeiStr = telephonyManager.getDeviceId();
+        return StringUtil.isEmpty(imeiStr) ? "" : imeiStr;
+    }
+
 }
